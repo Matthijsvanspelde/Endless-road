02a2d2f8ba72c332b8a84b88090843d489837f7f
Matthijs van Spelde <I404906@fhict.nl>
2020-03-16 12:12:44 +0100

    Initial commit

---
+++ Assets/Scripts/GenerateRoad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GenerateRoad : MonoBehaviour
+{
+    private IEnumerator coroutine;
+    public GameObject road;
+    public float interval;
+
+    void Start()
+    {
+        GameObject clone = Instantiate(road, new Vector3(0, 0, 0), Quaternion.identity);
+        coroutine = WaitAndInstantiate(interval);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator WaitAndInstantiate(float waitTime)
+    {
+        var inc = 10;
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            GameObject clone = Instantiate(road, new Vector3(0, 0, inc), Quaternion.identity);
+            Destroy(clone, 10);
+            print("Road generated");
+            inc += 10;
+        }
+        
+    }
+}
